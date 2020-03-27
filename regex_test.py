ae3456a496a4a9ca0a4cb3094bfbf534dae46d2d
CORP\Svyatoslav.Kovalev <Svyatoslav.Kovalev@acronis.com>
2020-03-27 18:35:50 +0300

    printf to begin of the functions

--- regex_test.py
+++ regex_test.py
@@ -1,20 +1,38 @@
 import re
 import os
 
-for  root, dirs,files in os.walk('C:\\Users\\Svyatoslav.Kovalev\\Documents\\avc'):
+reobj = re.compile("^[ ]*[^\n()]+[ ]+[^\n()]+[::]*[a-zA-Z]+\([^;{]*\n[ ]*{", flags=re.MULTILINE)
+for  root, dirs,files in os.walk('C:\\Users\\Svyatoslav.Kovalev\\Documents\\avc_regex_test\\avc\\avc'):
     for file in files:
       try:
         if ".cpp" in file:
+          pos = 0
+          allfile = ""
           with open(root + "\\" + file, 'r',errors='ignore') as f:
             allfile = f.read()
-            print(root + "\\" + file,":")
-            for item in re.finditer("^[ ]*[^\n()]+[ ]+[^\n()]+[::]*[a-zA-Z]+\([^;{]*\n[ ]*{", allfile, flags=re.MULTILINE):
+            #print(root + "\\" + file,":")
+            item = reobj.search(allfile, pos)
+            while item != None:
+              #print(item.start(), item.end())
               tmp = item[0]
               if "catch" in tmp or "switch" in tmp or "class" in tmp or "BOOST_SCOPE_EXIT" in tmp or "if" in tmp or "return" in tmp:
+                pos = item.end()
+                item = reobj.search(allfile, pos)
                 continue
               else:
-                print(item[0])
-              input(" ")
+                bfr = allfile[:item.end()]
+                nsrt = "printf(\"Hallo\\n\");"
+                tl = allfile[item.end():]
+                newallfile = bfr
+                newallfile += nsrt
+                newallfile += tl
+                allfile = newallfile
+                pos = item.end() + len(nsrt)
+                item = reobj.search(allfile, pos)
+                #print(item[0])
+              #input(" ")
+          with open(root + "\\" + file, 'w',errors='ignore') as f:
+            f.write(allfile)
       except IOError as ioerror:
         print(ioerror.args)
 
