afcd9881ff182372965ab00c7e201bc7f5349045
Svyatoslav.Kovalev <Svyatoslav.Kovalev@acronis.com>
2020-04-22 20:42:18 +0300

    class DUmmyTracer has been added

--- regex_test.py
+++ regex_test.py
@@ -10,9 +10,24 @@ def extract_function_name(template):
     last_not_white_space_pos = last_not_white_space_pos + 1
   return template[last_not_white_space_pos:bracket_pos]
 
-def trace_at_begin(allfile, item, reobj, what_to_print):
+def insert_dummy_tracer(allfile):
+  pos = allfile.find("#include")
+  prev_pos = pos
+  while pos != -1:
+    prev_pos = pos
+    pos = allfile.find("#include", prev_pos+1)
+  pos = allfile.find("\n", prev_pos)
+  nsrt="class DummyTracer\n{\npublic:\n  DummyTracer(const wchar_t* msg):\n    m_msg(msg)\n  {\n	m_threadId = GetCurrentThreadId();\n    SYSTEMTIME time;\n    GetLocalTime(&time);\n    TRACE(L\"(%d) %s started at %d:%d:%d:%d\\n\", m_threadId, m_msg.c_str(), time.wHour, time.wMinute, time.wSecond, time.wMilliseconds);\n  }\n  ~DummyTracer()\n  {\n    SYSTEMTIME time;\n    GetLocalTime(&time);\n    TRACE(L\"(%d) (%s) finished at %d:%d:%d:%d\\n\", m_threadId, m_msg.c_str(), time.wHour, time.wMinute, time.wSecond, time.wMilliseconds);\n  }\nprivate:\n  DWORD m_threadId;\n  std::wstring m_msg;\n};"
+  bfr = allfile[:pos+1]
+  tl = allfile[pos+1:]
+  newallfile = bfr
+  newallfile += nsrt
+  newallfile += tl
+  return newallfile
+
+def trace_at_begin(allfile, item, what_to_print):
   bfr = allfile[:item.end()]
-  nsrt = "TRACE(L\"%d {}\\n\", GetCurrentThreadId());".format(what_to_print)
+  nsrt = "DummyTracer dt(L\"{}\");".format(what_to_print)
   tl = allfile[item.end():]
   newallfile = bfr
   newallfile += nsrt
@@ -62,7 +77,7 @@ def trace_at_end(allfile, pos, what_to_print):
 
 def add_trace():
   reobj = re.compile("^[ ]*[^\n()]+[ ]+[^\n()]+[::]*[a-zA-Z]+\([^;{]*\n[ ]*{", flags=re.MULTILINE)
-  for  root, dirs,files in os.walk('C:\\Users\\Svyatoslav.Kovalev\\Documents\\tib-mounter-threadpool\\tib-mounter\\kernel\\win\\tib_mounter\\service'):
+  for  root, dirs,files in os.walk('C:\\Users\\Svyatoslav.Kovalev\\Documents\\tib-mounter\\kernel\\win\\tib_mounter\\service'):
       for file in files:
         try:
           if ".cpp" in file:
@@ -79,13 +94,14 @@ def add_trace():
                   continue
                 else:
                   fun_name = extract_function_name(item[0])
-                  inserted = trace_at_begin(allfile, item, reobj,fun_name)
+                  inserted = trace_at_begin(allfile, item, fun_name)
                   allfile = inserted[0]
                   pos = inserted[1]
                   #inserted = trace_at_end(allfile, pos, fun_name)
                   #allfile = inserted[0]
                   #pos = inserted[1]
                   item = reobj.search(allfile, pos)
+            allfile = insert_dummy_tracer(allfile)
             with open(root + "\\" + file, 'w',errors='ignore') as f:
               f.write(allfile)
         except IOError as ioerror:
